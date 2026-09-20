b249e5889432ebb95f3d89522c4c1a17897fea12
Quantum-mutnauQ <unconfigured@null.spigotmc.org>
2026-09-20 02:35:29 +0200

    added support for 26.3

---
+++ limitless options - NeoForge 26.3/src/main/java/quatum/limitless_options_neoforge/LimitlessOptions_NeoForge.java
@@ -0,0 +1,29 @@
+package quatum.limitless_options_neoforge;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import org.slf4j.Logger;
+
+import java.awt.*;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(LimitlessOptions_NeoForge.MODID)
+public class LimitlessOptions_NeoForge {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "limitless_options_neoforge";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "limitless_options_forge" namespace
+
+    public LimitlessOptions_NeoForge(IEventBus modEventBus, ModContainer modContainer) {
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC, "LimitlessOptions_NeoForge-Common.toml");
+
+    }
+}
+
